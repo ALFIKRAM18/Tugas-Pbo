7cab379ccfafb3ef5e1e4beb178786f70db1d5ca
alfikram hadi putra <131678872+ALFIKRAM18@users.noreply.github.com>
2023-06-01 23:36:33 +0700

    Create Kode.py

---
+++ Kode.py
@@ -0,0 +1,102 @@
+class Mahasiswa:
+    def __init__(self, nama, nim, jurusan):
+        self.nama = nama
+        self.nim = nim
+        self.jurusan = jurusan
+    
+    def tampilkan_info(self):
+        print("Nama:", self.nama)
+        print("NIM:", self.nim)
+        print("Jurusan:", self.jurusan.NamaJurusan)
+
+
+class Jurusan:
+    def __init__(self, nama_jurusan):
+        self.NamaJurusan = nama_jurusan
+        self.DaftarMahasiswa = []
+    
+    def tambah_mahasiswa(self, mahasiswa):
+        self.DaftarMahasiswa.append(mahasiswa)
+    
+    def tampilkan_daftar_mahasiswa(self):
+        print("Daftar Mahasiswa di Jurusan", self.NamaJurusan)
+        for mahasiswa in self.DaftarMahasiswa:
+            mahasiswa.tampilkan_info()
+
+
+class Universitas:
+    def __init__(self, nama_universitas):
+        self.NamaUniversitas = nama_universitas
+        self.DaftarJurusan = []
+    
+    def tambah_jurusan(self, jurusan):
+        self.DaftarJurusan.append(jurusan)
+    
+    def tampilkan_daftar_jurusan(self):
+        print("Daftar Jurusan di Universitas", self.NamaUniversitas)
+        for jurusan in self.DaftarJurusan:
+            print(jurusan.NamaJurusan)
+# 1. Implementasi kelas Mahasiswa, Jurusan, dan Universitas
+
+class Mahasiswa:
+    def __init__(self, nama, nim, jurusan):
+        self.nama = nama
+        self.nim = nim
+        self.jurusan = jurusan
+    
+    def tampilkan_info(self):
+        print("Nama:", self.nama)
+        print("NIM:", self.nim)
+        print("Jurusan:", self.jurusan.NamaJurusan)
+
+
+class Jurusan:
+    def __init__(self, nama_jurusan):
+        self.NamaJurusan = nama_jurusan
+        self.DaftarMahasiswa = []
+    
+    def tambah_mahasiswa(self, mahasiswa):
+        self.DaftarMahasiswa.append(mahasiswa)
+    
+    def tampilkan_daftar_mahasiswa(self):
+        print("Daftar Mahasiswa di Jurusan", self.NamaJurusan)
+        for mahasiswa in self.DaftarMahasiswa:
+            mahasiswa.tampilkan_info()
+
+
+class Universitas:
+    def __init__(self, nama_universitas):
+        self.NamaUniversitas = nama_universitas
+        self.DaftarJurusan = []
+    
+    def tambah_jurusan(self, jurusan):
+        self.DaftarJurusan.append(jurusan)
+    
+    def tampilkan_daftar_jurusan(self):
+        print("Daftar Jurusan di Universitas", self.NamaUniversitas)
+        for jurusan in self.DaftarJurusan:
+            print(jurusan.NamaJurusan)
+
+
+# Membuat objek Universitas XYZ
+nama_universitas = input("Masukkan nama universitas: ")
+universitas_xyz = Universitas(nama_universitas)
+
+# Membuat objek Jurusan Teknik Informatika dan menambahkannya ke dalam Universitas XYZ
+nama_jurusan = input("Masukkan nama jurusan: ")
+jurusan_ti = Jurusan(nama_jurusan)
+universitas_xyz.tambah_jurusan(jurusan_ti)
+
+# Memasukkan data mahasiswa ke dalam Jurusan Teknik Informatika di Universitas XYZ
+jumlah_mahasiswa = int(input("Masukkan jumlah mahasiswa: "))
+for i in range(jumlah_mahasiswa):
+    nama_mahasiswa = input("Masukkan nama mahasiswa: ")
+    nim_mahasiswa = input("Masukkan NIM mahasiswa: ")
+    mahasiswa = Mahasiswa(nama_mahasiswa, nim_mahasiswa, jurusan_ti)
+    jurusan_ti.tambah_mahasiswa(mahasiswa)
+
+# Menampilkan daftar jurusan di Universitas XYZ
+universitas_xyz.tampilkan_daftar_jurusan()
+
+# Menampilkan daftar mahasiswa yang terdaftar dalam Jurusan Teknik Informatika di Universitas XYZ
+jurusan_ti.tampilkan_daftar_mahasiswa()
